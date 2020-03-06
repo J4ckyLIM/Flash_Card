63546e2804175f056e7099637c2fa1a9882ce436
J4ckyLIM <jacky.lim@edu.itescia.fr>
2020-03-06 13:42:13 +0100

    Feat: result view

--- app/src/main/java/com/example/flash_card/ResultActivity.java
+++ app/src/main/java/com/example/flash_card/ResultActivity.java
@@ -1,22 +1,17 @@
 package com.example.flash_card;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
 public class ResultActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-
         final Intent intent = getIntent();
         int text = intent.getIntExtra("aString", 0);
         int result = intent.getIntExtra("aResult" , 0);
         TextView resultat = findViewById(R.id.ResultTextView);
-        resultat.setText(result + "/" + text);
+        resultat.setText(" Votre score est de : " +result + "/" + text + "");
     }
 }
